9097998144e730189e3c57d29a9369261325c7ab
scottlepp <scott.lepper@gmail.com>
2015-10-06 14:41:49 -0400

    PU-479 - path filters should work when jumping to classic

--- app/common/util/sugar.js
+++ app/common/util/sugar.js
@@ -66,9 +66,8 @@ angular.module('voyager.util').
                 retro = retro.replace(/%5C%2F/g, '%252F');
                 retro = retro.replace(/fq=/g, 'f.');
                 retro = retro.replace(/path_path/g, 'path');
-                // TODO these 2 seem backwards
-                //retro = retro.replace('f.path', 'path');
-                //retro = retro.replace('f.bbox', 'bbox');
+                retro = retro.replace('f.path', 'path');
+                retro = retro.replace('f.bbox', 'bbox');
                 retro = retro.replace(/:/g, '=');
                 return retro;
             },
