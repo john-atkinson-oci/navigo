85ced081a1b6b145685028e86d8b95822ea113fe
jason <jason@voyagersearch.com>
2015-10-12 10:24:09 -0700

    remove block comment.

--- test/e2e/zip-files-spec.js
+++ test/e2e/zip-files-spec.js
@@ -1,19 +1,5 @@
 'use strict';
-/*!
- * jQuery JavaScript Library v1.8.2
- * http://jquery.com/
- *
- * Includes Sizzle.js
- * http://sizzlejs.com/
- *
- * Copyright 2012 jQuery Foundation and other contributors
- * Released under the MIT license
- * http://jquery.org/license
- *
- * Date: Thu Sep 20 2012 21:13:05 GMT-0400 (Eastern Daylight Time)
- * Updated: Mon Oct 12 2015
- * Updated by: Jason Pardy
- */
+
 
 describe('Zip Files Task', function() {
 
