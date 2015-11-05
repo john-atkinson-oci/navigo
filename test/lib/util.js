c645051452ae135513aee048f7714c17733c342c
scottlepp <scott.lepper@gmail.com>
2015-11-05 17:01:56 -0500

    VG-3260 - remove page specific function.  moved to search page

--- test/lib/util.js
+++ test/lib/util.js
@@ -1,17 +1,6 @@
 var Util = (function () {
     'use strict';
 
-    function checkResultsCount(addToQueueAnchor) {
-        //Get the result total -- check if greater than 0.
-        var result_count = 0;
-        addToQueueAnchor.getText().then(function (text) {
-            var result_text = text.split(' ');
-            result_count = parseInt(result_text[0]);
-            expect(parseInt(result_count) > 0).toBeTruthy();
-            return text;
-        });
-    }
-
     return {
 
         waitForSpinner: function() {
@@ -47,28 +36,7 @@ var Util = (function () {
                 }
                 return !isVisible;
             });
-        },
-
-        addFileTypeToQueue: function(query) {
-            // Search for CA OZONE feature classes.
-            browser.get(this.getServer() + '#/search?q=' + query + '&disp=71f42f58&view=card');
-
-            this.waitForSpinner();
-            this.waitForSpinner();
-            this.waitForSpinner();  //TODO fix this it doesn't really block its a promise
-
-            // Login into Voyager
-            this.loginToVoyager('admin', 'admin');
-            browser.waitForAngular();
-
-            // Get the Add to Queue element
-            var addToQueueAnchor = element(by.css('.total.flyout_trigger.ng-binding'));
-            checkResultsCount(addToQueueAnchor);
-            // Add all items to the cart
-            browser.actions().mouseMove(addToQueueAnchor).click(addToQueueAnchor).perform();
-            element(by.css('[ng-click="addAllToCart()"]')).click();
         }
-
     };
 })();  // jshint ignore:line
 module.exports = Util;
