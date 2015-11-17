19863f3221a39d8e6d6b72b1111960008477d06b
scottlepp <scott.lepper@gmail.com>
2015-11-17 11:21:47 -0500

    VG-3260 -  jenkins phantomjs issue; leaflet?

--- test/spec/search/search-map-directive-spec.js
+++ test/spec/search/search-map-directive-spec.js
@@ -1,119 +1,120 @@
-///*global describe, beforeEach, module, it, inject, expect, angular, config */
-//
-//describe('Search Map Directive:', function () {
-//    'use strict';
-//
-//    beforeEach(function () {
-//        module('templates');
-//        // module('voyager.common'); //auth service module - apparently this is needed to mock the auth service
-//        module('voyager.search');
-//        module(function ($provide) {
-//            $provide.constant('config', config);
-//        });
-//    });
-//
-//    var scope, element, controller, compiled, timeout, httpMock, rootScope, compile, leafletData, $q;
-//
-//    $(document.body).append('<div id="search-map"></div>');
-//    var map = L.map('search-map');
-//
-//    function initDirective($rootScope, $compile) {
-//        element = angular.element('<div type="text" vs-search-map></div>');
-//        compiled = $compile(element)($rootScope);
-//        element.scope().$apply();
-//        controller = element.controller(scope);
-//    }
-//
-//    beforeEach(inject(function ($compile, $rootScope, $timeout, $httpBackend, _leafletData_, _$q_) {
-//        scope = $rootScope.$new();
-//        timeout = $timeout;
-//        httpMock = $httpBackend;
-//        rootScope = $rootScope;
-//        compile = $compile;
-//        leafletData = _leafletData_;
-//        $q = _$q_;
-//        spyOn(leafletData,'getMap').and.callFake(function() {
-//            return $q.when(map);
-//        });
-//    }));
-//
-//
-//    describe('Render', function () {
-//
-//        it('should render leaflet map', function () {
-//            initDirective(rootScope, compile);
-//
-//            timeout.flush();
-//
-//            expect(element.html()).toContain('leaflet');
-//        });
-//
-//        it('should toggle map mode', function () {
-//            rootScope.displayFormat = 'detail_format';
-//            initDirective(rootScope, compile);
-//            expect(element.hasClass('dragging_disabled')).toBeTruthy();
-//
-//            rootScope.displayFormat = 'short_format';
-//            rootScope.$apply();
-//
-//            timeout.flush();
-//
-//            expect(element.hasClass('dragging_disabled')).toBeFalsy();
-//        });
-//
-//    });
-//
-//    describe('Functions', function () {
-//
-//        it('should zoom', function () {
-//            initDirective(rootScope, compile);
-//
-//            scope.zoomIn($.Event( 'click' ));
-//
-//            scope.zoomOut($.Event( 'click' ));
-//        });
-//
-//        it('should toggle display format', function () {
-//            initDirective(rootScope, compile);
-//
-//            scope.toggleDisplayFormat('Card', $.Event('click'));
-//
-//            expect(scope.selectedMapType).toBe('Card');
-//        });
-//
-//        it('should draw rectangle', function () {
-//            rootScope.selectedDrawingType = 'Intersects';
-//            rootScope.displayFormat = 'detail_format';
-//
-//            initDirective(rootScope, compile);
-//
-//            scope.drawRectangle('within', $.Event('click'));
-//
-//            scope.$apply();
-//            timeout.flush();
-//
-//            expect(scope.selectedDrawingType).toBe('Within');
-//            expect(leafletData.getMap).toHaveBeenCalled();
-//
-//        });
-//
-//        it('should resize', function () {
-//            rootScope.selectedDrawingType = 'Intersects';
-//            rootScope.displayFormat = 'detail_format';
-//
-//            // TODO function in outer scope, fix this
-//            rootScope.toggleMap = function(){};
-//
-//            initDirective(rootScope, compile);
-//
-//            scope.resize();
-//
-//            scope.$apply();
-//            timeout.flush();
-//
-//            expect(scope.selectedDrawingType).toBe('Intersects');
-//            expect(leafletData.getMap).toHaveBeenCalled();
-//        });
-//
-//    });
-//});
+describe('Search Map Directive:', function () {
+    'use strict';
+
+    var cfg = _.clone(config);
+
+    beforeEach(function () {
+        module('templates');
+        // module('voyager.common'); //auth service module - apparently this is needed to mock the auth service
+        module('voyager.search');
+        module(function ($provide) {
+            $provide.constant('config', cfg);
+        });
+    });
+
+    //var scope, element, controller, compiled, timeout, httpMock, rootScope, compile, leafletData, $q;
+
+    $(document.body).append('<div id="search-map"></div>');
+    var map = L.map('search-map');
+
+    console.log('createed map' + map.id);
+    //function initDirective($rootScope, $compile) {
+    //    element = angular.element('<div type="text" vs-search-map></div>');
+    //    compiled = $compile(element)($rootScope);
+    //    element.scope().$apply();
+    //    controller = element.controller(scope);
+    //}
+    //
+    //beforeEach(inject(function ($compile, $rootScope, $timeout, $httpBackend, _leafletData_, _$q_) {
+    //    scope = $rootScope.$new();
+    //    timeout = $timeout;
+    //    httpMock = $httpBackend;
+    //    rootScope = $rootScope;
+    //    compile = $compile;
+    //    leafletData = _leafletData_;
+    //    $q = _$q_;
+    //    spyOn(leafletData,'getMap').and.callFake(function() {
+    //        return $q.when(map);
+    //    });
+    //}));
+    //
+    //
+    //describe('Render', function () {
+    //
+    //    it('should render leaflet map', function () {
+    //        initDirective(rootScope, compile);
+    //
+    //        timeout.flush();
+    //
+    //        expect(element.html()).toContain('leaflet');
+    //    });
+    //
+    //    it('should toggle map mode', function () {
+    //        rootScope.displayFormat = 'detail_format';
+    //        initDirective(rootScope, compile);
+    //        expect(element.hasClass('dragging_disabled')).toBeTruthy();
+    //
+    //        rootScope.displayFormat = 'short_format';
+    //        rootScope.$apply();
+    //
+    //        timeout.flush();
+    //
+    //        expect(element.hasClass('dragging_disabled')).toBeFalsy();
+    //    });
+    //
+    //});
+    //
+    //describe('Functions', function () {
+    //
+    //    it('should zoom', function () {
+    //        initDirective(rootScope, compile);
+    //
+    //        scope.zoomIn($.Event( 'click' ));
+    //
+    //        scope.zoomOut($.Event( 'click' ));
+    //    });
+    //
+    //    it('should toggle display format', function () {
+    //        initDirective(rootScope, compile);
+    //
+    //        scope.toggleDisplayFormat('Card', $.Event('click'));
+    //
+    //        expect(scope.selectedMapType).toBe('Card');
+    //    });
+    //
+    //    it('should draw rectangle', function () {
+    //        rootScope.selectedDrawingType = 'Intersects';
+    //        rootScope.displayFormat = 'detail_format';
+    //
+    //        initDirective(rootScope, compile);
+    //
+    //        scope.drawRectangle('within', $.Event('click'));
+    //
+    //        scope.$apply();
+    //        timeout.flush();
+    //
+    //        expect(scope.selectedDrawingType).toBe('Within');
+    //        expect(leafletData.getMap).toHaveBeenCalled();
+    //
+    //    });
+    //
+    //    it('should resize', function () {
+    //        rootScope.selectedDrawingType = 'Intersects';
+    //        rootScope.displayFormat = 'detail_format';
+    //
+    //        // TODO function in outer scope, fix this
+    //        rootScope.toggleMap = function(){};
+    //
+    //        initDirective(rootScope, compile);
+    //
+    //        scope.resize();
+    //
+    //        scope.$apply();
+    //        timeout.flush();
+    //
+    //        expect(scope.selectedDrawingType).toBe('Intersects');
+    //        expect(leafletData.getMap).toHaveBeenCalled();
+    //    });
+    //
+    //});
+});
