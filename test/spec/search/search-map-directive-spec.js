c1c7e95f24a7b51f59c7bfd638fb7209a8731ffd
scottlepp <scott.lepper@gmail.com>
2015-11-17 12:31:07 -0500

    VG-3260 -  jenkins phantomjs issue; leaflet collision?

--- test/spec/search/search-map-directive-spec.js
+++ test/spec/search/search-map-directive-spec.js
@@ -56,65 +56,61 @@ describe('Search Map Directive:', function () {
         });
     });
 
+    describe('Functions', function () {
 
+        it('should zoom', function () {
+            initDirective(rootScope, compile);
 
-    //
-    //
-    //describe('Functions', function () {
-    //
-    //    it('should zoom', function () {
-    //        initDirective(rootScope, compile);
-    //
-    //        scope.zoomIn($.Event( 'click' ));
-    //
-    //        scope.zoomOut($.Event( 'click' ));
-    //    });
-    //
-    //    it('should toggle display format', function () {
-    //        initDirective(rootScope, compile);
-    //
-    //        scope.toggleDisplayFormat('Card', $.Event('click'));
-    //
-    //        expect(scope.selectedMapType).toBe('Card');
-    //    });
-    //
-    //    it('should draw rectangle', function () {
-    //        rootScope.selectedDrawingType = 'Intersects';
-    //        rootScope.displayFormat = 'detail_format';
-    //
-    //        spyOn(leafletData,'getMap').and.callThrough();
-    //
-    //        initDirective(rootScope, compile);
-    //
-    //        scope.drawRectangle('within', $.Event('click'));
-    //
-    //        scope.$apply();
-    //        $timeout.flush();
-    //
-    //        expect(scope.selectedDrawingType).toBe('Within');
-    //        expect(leafletData.getMap).toHaveBeenCalled();
-    //
-    //    });
-    //
-    //    it('should resize', function () {
-    //        rootScope.selectedDrawingType = 'Intersects';
-    //        rootScope.displayFormat = 'detail_format';
-    //
-    //        spyOn(leafletData,'getMap').and.callThrough();
-    //
-    //        // TODO function in outer scope, fix this
-    //        rootScope.toggleMap = function(){};
-    //
-    //        initDirective(rootScope, compile);
-    //
-    //        scope.resize();
-    //
-    //        scope.$apply();
-    //        $timeout.flush();
-    //
-    //        expect(scope.selectedDrawingType).toBe('Intersects');
-    //        expect(leafletData.getMap).toHaveBeenCalled();
-    //    });
-    //
-    //});
+            scope.zoomIn($.Event( 'click' ));
+
+            scope.zoomOut($.Event( 'click' ));
+        });
+
+        //it('should toggle display format', function () {
+        //    initDirective(rootScope, compile);
+        //
+        //    scope.toggleDisplayFormat('Card', $.Event('click'));
+        //
+        //    expect(scope.selectedMapType).toBe('Card');
+        //});
+        //
+        //it('should draw rectangle', function () {
+        //    rootScope.selectedDrawingType = 'Intersects';
+        //    rootScope.displayFormat = 'detail_format';
+        //
+        //    spyOn(leafletData,'getMap').and.callThrough();
+        //
+        //    initDirective(rootScope, compile);
+        //
+        //    scope.drawRectangle('within', $.Event('click'));
+        //
+        //    scope.$apply();
+        //    $timeout.flush();
+        //
+        //    expect(scope.selectedDrawingType).toBe('Within');
+        //    expect(leafletData.getMap).toHaveBeenCalled();
+        //
+        //});
+        //
+        //it('should resize', function () {
+        //    rootScope.selectedDrawingType = 'Intersects';
+        //    rootScope.displayFormat = 'detail_format';
+        //
+        //    spyOn(leafletData,'getMap').and.callThrough();
+        //
+        //    // TODO function in outer scope, fix this
+        //    rootScope.toggleMap = function(){};
+        //
+        //    initDirective(rootScope, compile);
+        //
+        //    scope.resize();
+        //
+        //    scope.$apply();
+        //    $timeout.flush();
+        //
+        //    expect(scope.selectedDrawingType).toBe('Intersects');
+        //    expect(leafletData.getMap).toHaveBeenCalled();
+        //});
+
+    });
 });
