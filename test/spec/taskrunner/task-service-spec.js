e13ceb81285b0d1093cd98d2cdd270cf26f25a23
scottlepp <scott.lepper@gmail.com>
2015-11-19 09:10:06 -0500

    VG-3260 - task service

---
+++ test/spec/taskrunner/task-service-spec.js
@@ -0,0 +1,62 @@
+'use strict';
+
+describe('Factory: taskService', function () {
+
+    var $http, $q, taskService;
+
+    var cfg = _.clone(config);
+
+    beforeEach(function () {
+        module('taskRunner');
+        module(function ($provide) {
+            $provide.constant('config', cfg);
+        });
+
+        inject(function (_$httpBackend_, _$q_, _taskService_) {
+            $http = _$httpBackend_;
+            $q = _$q_;
+            taskService = _taskService_;
+        });
+    });
+
+    // Specs here
+
+    it('should get files', function () {
+
+        var output = {children:[{format:'format', name:'name'}]};
+        var statusResponse = {data:{id:'task', output:output}};
+        var files = taskService.getFiles(statusResponse);
+
+        expect(files.length).toBe(1);
+        expect(files[0].downloadUrl).toContain(new RegExp('task\/output') + 'name');
+    });
+
+    it('should get log files', function () {
+
+        var output = {children:[{format:'format', name:'_stderr'},{format:'format', name:'_stdout'}]};
+        var statusResponse = {data:{id:'task', output:output}};
+        var files = taskService.getLogFiles(statusResponse);
+
+        expect(files.length).toBe(2);
+        expect(files[0].downloadUrl).toContain(new RegExp('task\/output') + '_stderr');
+        expect(files[1].downloadUrl).toContain(new RegExp('task\/output') + '_stdout');
+
+        expect(files[0].displayName).toBe('Processing Error Log');
+        expect(files[1].displayName).toBe('Processing Output');
+    });
+
+    it('should get report', function () {
+
+        var output = {children:[{format:'format', name:'_report'}]};
+        var statusResponse = {data:{id:'task', output:output}};
+
+        $http.expectGET(new RegExp('report')).respond({'report':'report'});
+
+        taskService.getReport(statusResponse).then(function(data) {
+            expect(data.report).toBe('report');
+        });
+
+        $http.flush();
+
+    });
+});
