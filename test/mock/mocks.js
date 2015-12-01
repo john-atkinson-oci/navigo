cc12a6d5573c228de2a7f1785d5e43812bf4f12c
nebulae <nebulae@gmail.com>
2015-12-01 12:45:57 -0800

    VG-3515 added saved content modal test directive,
    added a response mocks object to the mocks.js for storing mocked son
    respondes.

--- test/mock/mocks.js
+++ test/mock/mocks.js
@@ -83,3 +83,25 @@ map = $.extend(map,new LeafletMapMock());
 function escapeRegExp(str) {
     return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&');
 }
+
+function ResponseMocks(){
+    return {
+        mockSavedSearchesResponse : {
+            'responseHeader': {
+                'status': 0,
+                'QTime': 0,
+                'params': { 'rand': '0.03541157837025821', 'json.wrf': 'angular.callbacks._4', 'rows': '150', 'wt': 'json' }
+            },
+            'security': [ 'query', 'share:(_EVERYONE _LOGGEDIN _ADMIN) OR owner:admin' ],
+            'response': {
+                'numFound': 17,
+                'start': 0,
+                'docs': [
+                    { 'id': 'S1511B388E58', 'title': 'Voyager Default View', 'description': 'Voyager\'s default settings', 'owner': 'admin', 'path': '/disp=e93d0356/', 'share': [ '_EVERYONE' ], 'order': 1427453990000, 'saved': '2013-09-16T19:17:26Z', 'private': false, 'config': 'e93d0356', 'view': 'SUMMARY', 'query': '', '_version_': 1518191975143047200},
+                    { 'id': 'S1511B388E57', 'title': 'Raster Properties', 'description': 'List of raster images with assorted properties.', 'owner': 'admin', 'path': '/f.geometry_type=Raster/view=GRID/disp=c13b4edb/', 'share': ['_EVERYONE'],'order': 1427453980000,'saved': '2010-07-21T22:25:45Z','private': false,'config': 'c13b4edb','view': 'GRID','param_fq': ['geometry_type:Raster'],'query': 'fq=geometry_type:Raster&', '_version_': 1518191974979469300 }
+                ]
+            }
+        } 
+    }
+}
+
