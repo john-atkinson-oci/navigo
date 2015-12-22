f8b780954be1c8c3b62ca90f10fa34f85d29e054
jason <jason@voyagersearch.com>
2015-12-22 12:03:48 -0800

    VG-3565: style changes and new tests

--- app/src/taskrunner/tasks-controller.js
+++ app/src/taskrunner/tasks-controller.js
@@ -16,11 +16,14 @@ angular.module('taskRunner')
                 usSpinnerService.stop('tasks-spinner');
                 $scope.refreshing = false;
 
-                //_.each($scope.taskList, function(item){item.available = false;});  //test all unavailable
-
-                var unavailableTasks = _.filter($scope.taskList, function(task){return task.available === false;});
+                var unavailableTasks = [];
+                var taskCount = 0;
+                angular.forEach($scope.taskList, function(tasks) {
+                    unavailableTasks = unavailableTasks.concat(_.filter(tasks, function(task){return task.available === false;}));
+                    taskCount += tasks.length;
+                });
                 $scope.hasUnavailable = unavailableTasks.length > 0;
-                $scope.allUnavailable = unavailableTasks.length === $scope.taskList.length;
+                $scope.allUnavailable = unavailableTasks.length === taskCount;
                 if ($scope.allUnavailable) {
                     $scope.toggleTasksText = 'Show Available'; // will show all tasks, button text is opposite (toggle)
                 }
@@ -39,37 +42,26 @@ angular.module('taskRunner')
             $state.go('task', {task: task});
         };
 
-        //$scope.searchParams = '&' + $.param($routeParams,true);
-
-        $scope.getIcon = function(task) {
-            return task.available ? 'glyphicon-ok-circle':'glyphicon-ban-circle';
-        };
-
-        $scope.getStyle = function(task) {
-            var style = 'cursor:' +  (task.available?'pointer;':'not-allowed;');
-            style += 'color:' + (task.available?'':'#bbb;');
-            return style;
-        };
-
         authService.getPrivileges().then(function() {
             $scope.canReload = authService.hasPermission('manage');
             _loadTasks();
         });
 
-        $scope.toggleFilter = function() {
-            if ($scope.toggleTasksText === 'Show Available') {
-                $scope.toggleTasksText = 'Show All';
-            } else {
-                $scope.toggleTasksText = 'Show Available';
-            }
-        };
+        // Currently not used.
+        //$scope.toggleFilter = function() {
+        //    if ($scope.toggleTasksText === 'Show Available') {
+        //        $scope.toggleTasksText = 'Show All';
+        //    } else {
+        //        $scope.toggleTasksText = 'Show Available';
+        //    }
+        //};
 
-        $scope.filterTask = function(task) {
-            var match = true;
-            if ($scope.toggleTasksText === 'Show All') {  //means currently showing only available
-                match = task.available;
-            }
-            return match;
-        };
+        //$scope.filterTask = function(task) {
+        //    var match = true;
+        //    if ($scope.toggleTasksText === 'Show All') {  //means currently showing only available
+        //        match = task.available;
+        //    }
+        //    return match;
+        //};
 
     });
