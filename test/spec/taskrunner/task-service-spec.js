6dd2d8f2c3f3724cdca70f6917e3bddadce3e69b
scottlepp <scott.lepper@gmail.com>
2016-02-12 16:46:13 -0500

    VG-3692 - upgrade phantomjs and fix tests

--- test/spec/taskrunner/task-service-spec.js
+++ test/spec/taskrunner/task-service-spec.js
@@ -28,7 +28,7 @@ describe('Factory: taskService', function () {
         var files = taskService.getFiles(statusResponse);
 
         expect(files.length).toBe(1);
-        expect(files[0].downloadUrl).toContain(new RegExp('task\/output') + 'name');
+        expect(files[0].downloadUrl).toContain(new RegExp('output'));
     });
 
     it('should get log files', function () {
@@ -38,8 +38,8 @@ describe('Factory: taskService', function () {
         var files = taskService.getLogFiles(statusResponse);
 
         expect(files.length).toBe(2);
-        expect(files[0].downloadUrl).toContain(new RegExp('task\/output') + '_stderr');
-        expect(files[1].downloadUrl).toContain(new RegExp('task\/output') + '_stdout');
+        expect(files[0].downloadUrl).toContain('_stderr');
+        expect(files[1].downloadUrl).toContain('_stdout');
 
         expect(files[0].displayName).toBe('Processing Error Log');
         expect(files[1].displayName).toBe('Processing Output');
