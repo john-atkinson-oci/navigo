5291149461978b643c6a6ba1b90f6742e6634e6a
jason <jason@voyagersearch.com>
2016-03-09 16:00:37 -0800

    Change the number of fields returned from 500 to 10000.

--- app/common/fields/field-service.js
+++ app/common/fields/field-service.js
@@ -6,7 +6,7 @@ angular.module('voyager.fields').
 
         return {
             fetchFields: function () {
-                return $http.jsonp(config.root + 'solr/fields/select?q=*:*&wt=json&rows=500&json.wrf=JSON_CALLBACK').then(function(res) {
+                return $http.jsonp(config.root + 'solr/fields/select?q=*:*&wt=json&rows=10000&json.wrf=JSON_CALLBACK').then(function(res) {
                     return res.data.response.docs;
                 });
             }
