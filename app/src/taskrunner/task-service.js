22541bc67de7cae8fe3a8ff90a96fd4f2b2cd764
jason <jason@voyagersearch.com>
2016-03-18 11:45:26 -0700

    VG-3859: support for defaultTask

--- app/src/taskrunner/task-service.js
+++ app/src/taskrunner/task-service.js
@@ -5,9 +5,8 @@ angular.module('taskRunner').
         'use strict';
 
         var _items = [];
-        var defaultTaskName = config.defaultTask;
         var errorMessage = 'All the items in the cart are invalid formats';
-        if (!_.isEmpty(config.defaultTask)) {
+        if (angular.isDefined(config.defaultTask) && !_.isEmpty(config.defaultTask)) {
             errorMessage = 'All the items in the cart are invalid formats for: ' + _.classify(config.defaultTask);
         }
 
